a3d91e4f8525291a792cdedff756e5b4a9eb68a6
Nicolás Snider <nicolas.snider@outlook.com>
2026-02-14 18:50:15 -0600

    Scaffold villa data model and EF Core integration
    Add domain, application, and infrastructure projects for clean architecture.
    
    Define Villa entity and ApplicationDbContext with seed data.
    
    Set up Entity Framework Core with SQL Server, add initial migrations, and update web project for DI and configuration.
    
    Update solution, .editorconfig, .gitignore, and environment settings.

---
+++ WhiteLagoon.Infrastructure/Migrations/20260215004501_update_villas.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_villas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Pccupancy",
+                table: "Villas",
+                newName: "Occupancy");
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2026, 2, 14, 18, 45, 1, 17, DateTimeKind.Local).AddTicks(9045), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", "Royal Villa", 4, 200.0, 550, null },
+                    { 2, new DateTime(2026, 2, 14, 18, 45, 1, 17, DateTimeKind.Local).AddTicks(9065), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x401", "Premium Pool Villa", 4, 300.0, 550, null },
+                    { 3, new DateTime(2026, 2, 14, 18, 45, 1, 17, DateTimeKind.Local).AddTicks(9067), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x402", "Luxury Pool Villa", 4, 400.0, 750, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Occupancy",
+                table: "Villas",
+                newName: "Pccupancy");
+        }
+    }
+}
